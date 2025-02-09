953befd86c8f8eb07e2db20c84f70dac1c01d641
Rafael <rafaelsoares.p.f@gmail.com>
2025-02-09 02:15:47 -0300

    html e style concluido, iniciandoo js

---
+++ Exercício01/script.js
@@ -0,0 +1,12 @@
+let selecionado = []
+
+function selecionar(numElemento){
+    
+    let cursos=document.getElementsByClassName("curso c1")
+    console.log(cursos)
+    cursos[numElemento].innerText = "clicou"
+
+}
+
+function copiar(){
+}
