b377ee2565c7beea04c08aa2160eb45da9dd3f64
Rafael <rafaelsoares.p.f@gmail.com>
2025-02-09 17:38:25 -0300

    função selecionar funcionando

--- Exercício01/script.js
+++ Exercício01/script.js
@@ -1,11 +1,34 @@
-let selecionado = []
+let selecionado = [false,false,false,false,false,false]
 
 function selecionar(numElemento){
     
     let cursos=document.getElementsByClassName("curso c1")
-    console.log(cursos)
-    cursos[numElemento].innerText = "clicou"
 
+    //se elemento não estiver selecionado
+    if(!selecionado[numElemento]){
+
+        //alterar o estilo
+        cursos[numElemento].classList.add("destaque")
+        //adicionar elemento no vetor "selecionado"
+        selecionado[numElemento] = true
+
+    }else{
+
+        //remover estilo
+        cursos[numElemento].classList.remove("destaque")
+        //elemento recebe false - não selecionado
+        selecionado[numElemento] = false
+       
+    }
+}
+
+function removerSelecionado(numElemento){
+    for(let contador in selecionado){
+        //se for igual
+        if(selecionado[contador] == numElemento){
+            selecionado[contador] = selecionado[contador+1 ]
+        }
+    }
 }
 
 function copiar(){
