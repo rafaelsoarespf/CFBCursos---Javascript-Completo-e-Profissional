e3bbc21e22afc287e77d16c57c31ca07d0b54d35
Rafael Soares de Paula FIlho <rafaelsoares.p.f@gmail.com>
2025-04-14 20:57:52 -0300

    refeito o projeto usando map

--- Exercício01/script.js
+++ Exercício01/script.js
@@ -1,46 +1,20 @@
-let selecionado = [false,false,false,false,false,false]
+const cursos = [...document.querySelectorAll(".curso")];
+const btnCopiar= document.querySelector("#btn_copiar");
 
-function selecionar(numElemento){
-    
-    let cursos=document.getElementsByClassName("curso c1")
+const caixa2 = document.querySelector("#caixa2")
 
-    //se elemento não estiver selecionado
-    if(!selecionado[numElemento]){
-
-        //alterar o estilo
-        cursos[numElemento].classList.add("destaque")
-        //adicionar elemento no vetor "selecionado"
-        selecionado[numElemento] = true
-
-    }else{
-
-        //remover estilo
-        cursos[numElemento].classList.remove("destaque")
-        //elemento recebe false - não selecionado
-        selecionado[numElemento] = false
-       
-    }
-}
-
-function copiar(){
-    
-    //elementos
-    let caixa2 = document.getElementById("caixa2")
-    let cursos=document.getElementsByClassName("curso c1")
-
-    //limpando
-    caixa2.innerHTML = ''
-
-    //criando elemento
-    for(contador in selecionado){
-        if(selecionado[contador]){
-            novoElemento = document.createElement("div")
-            novoElemento.setAttribute('class', 'curso c1')
-            novoElemento.innerText = cursos[contador].innerText
-            caixa2.appendChild(novoElemento)
+btnCopiar.addEventListener('click',()=>{
+    cursos.map((curso, indice) => {
+        if(curso.classList.contains("destaque")){
+            caixa2.appendChild(curso)
+            curso.classList.remove("destaque");
         }
-    }
-    
+    })
+})
 
-
-}
+//altera a cor quando clica no curso para cor da class .destaque
+cursos.map((curso,indice)=>{
+    curso.addEventListener('click', ()=>{
+        curso.classList.toggle("destaque");
+    })
+})
